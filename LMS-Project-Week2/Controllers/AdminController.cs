46fb96ddc3878fc080c6eac8a634f4156d9d6749
Abdulelah <AbdulelahAng@gmail.com>
2023-06-10 23:21:35 +0300

    Last version

--- LMS-Project-Week2/Controllers/AdminController.cs
+++ LMS-Project-Week2/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using LMS_Project_Week2.CommonClass;
 
 namespace LMS_Project_Week2.Controllers
 {
@@ -12,38 +14,86 @@ namespace LMS_Project_Week2.Controllers
     {
         public static readonly Crud_Operation operation = Crud_Operation.Instance;
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Index(int? page, string searchString)
         {
+            if (System.Web.HttpContext.Current.Session[AuthinticationLoginSession.loggedInUserObject] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            int pageSize = 5;
+            int pageNum;
+            if (page == null)
+            {
+                pageNum = (page ?? 1);
+            }
+            else
+            {
+                pageNum = (int)page;
+            }
+
             var books = new List<Book>();
+            var client = new Client();
             books = operation.GetList();
-            return View(books);
+            foreach (Book item in books)
+            {
+                item.author = operation.GetAuthor(item.AuthorId);
+                if (item.IsAvailable != "Yes")
+                {
+                    item.client = operation.GetClient(item.Id);
+                }
+            }
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = (List<Book>)books.Where(b => b.Title.Contains(searchString)).ToList();
+            }
+
+            return View(books.ToList().ToPagedList(pageNum, pageSize));
         }
 
         // GET: Admin/Details/5
         public ActionResult Details(int id)
         {
+            if (System.Web.HttpContext.Current.Session[AuthinticationLoginSession.loggedInUserObject] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
             var books = new List<Book>();
             books = operation.GetList();
+            foreach (var item in books)
+            {
+                item.author = operation.GetAuthor(item.AuthorId);
+                if (item.IsAvailable.ToLower() == "no")
+                    item.client = operation.GetClient(item.Id);
+            }
             return View(books.Find(x => x.Id == id));
         }
 
         // GET: Admin/Create
         public ActionResult Create()
         {
+            if (System.Web.HttpContext.Current.Session[AuthinticationLoginSession.loggedInUserObject] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            ViewBag.Authors = operation.GetAuthors();            
             return View();
         }
 
         // POST: Admin/Create
         [HttpPost]
-        public ActionResult Create(Book book, string author)
+        public ActionResult Create(Book book)
         {
             try
             {
-                if (ModelState.IsValid)
+                //if (ModelState.IsValid)
                 {
                     Author authorInfo = new Author();
-                    authorInfo.Name = author;
-                    if(operation.Add_Book(book, authorInfo))
+                    authorInfo.Name = book.author.Name;
+                    book.AuthorId = book.author.Id;
+                    if (operation.Add_Book(book, authorInfo))
                     {
                         ViewBag.Message = "Book Details Added Successfully";
                         ModelState.Clear();
@@ -61,7 +111,20 @@ namespace LMS_Project_Week2.Controllers
         // GET: Admin/Edit/5
         public ActionResult Edit(int id)
         {
+            if (System.Web.HttpContext.Current.Session[AuthinticationLoginSession.loggedInUserObject] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
             var books = operation.GetList();
+            ViewBag.Authors = operation.GetAuthors();
+            foreach (var item in books)
+            {
+                item.author = operation.GetAuthor(item.AuthorId);
+                if (item.IsAvailable.ToLower() == "no")
+                    item.client = operation.GetClient(item.Id);
+                else
+                    ViewBag.client = operation.GetFreeClient();
+            }
             return View(books.Find(x => x.Id == id));
         }
 
@@ -72,13 +135,18 @@ namespace LMS_Project_Week2.Controllers
             try
             {
                 book.Id = id;
+                if(book.IsAvailable.ToLower() == "yes" && book.client.Id != 0 && book.client.Id != null)
+                {
+                    book.IsAvailable = operation.UpdateClientBookId(book) ? "No" : "Yes";
+                }
+
                 if (operation.UpdateBook(book))
                 {
                     return RedirectToAction("Index");
                 }
                 return View();
             }
-            catch
+            catch(Exception ex)
             {
                 return View();
             }
@@ -87,18 +155,30 @@ namespace LMS_Project_Week2.Controllers
         // GET: Admin/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            if (System.Web.HttpContext.Current.Session[AuthinticationLoginSession.loggedInUserObject] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var books = operation.GetList();
+            foreach(var item in books)
+            {
+                item.author = operation.GetAuthor(item.AuthorId);
+            }
+            return View(books.Find(x => x.Id == id));
         }
 
         // POST: Admin/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id, Book book)
         {
             try
             {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
+                book.Id = id;
+                if (operation.DeleteBook(book))
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
             }
             catch
             {
